299fe9a5002ffa40844a8d61c71eb12e659ef2ed
Sergey Kostyuk <contact@s-kostyuk.me>
2016-11-21 00:13:42 +0200

    Added updated version of second lab

---
+++ lab2_v2_new/lab2_v2_new/UniversityStudent.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_v2_new
+{
+
+    abstract class Mark
+    {
+        public static void CheckIsValid(int _value)
+        {
+            if (_value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Mark can't be negative", "_value");
+            }
+            else if (_value > 5)
+            {
+                throw new ArgumentOutOfRangeException("Mark can't be bigger than 5", "_value");
+            }
+        }
+    }
+
+    abstract class Scholarship
+    {
+        public static bool IsAvailable(int[] marks) {
+            return (marks.Average() >= 4);
+        }
+
+    }
+
+    class UniversityStudent
+    {
+        /************************************************/
+
+        public const ushort N_OF_SUBJECTS = 10;
+
+        /************************************************/
+
+        public string name { get; private set; }
+
+        public string surname { get; private set; }
+
+        public string patronymic { get; private set; }
+
+        public uint course { get; private set; }
+
+        public DateTime birthday { get; private set; }
+
+        private double m_scholarship;
+
+        public double scholarship
+        {
+            get
+            {
+                return m_scholarship;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Scholarship value cant't be negative", "value");
+                }
+                m_scholarship = value;
+            }
+        }
+
+        public double balance { get; private set; }
+
+        // Тут так и просится ushort. Но ushort и short не поддерживаются в LINQ
+        private int[] marks;
+
+        /************************************************/
+
+        public UniversityStudent(
+            string _name, string _surname, string _patronymic, 
+            uint _course, DateTime _birthday, double _scholarship, 
+            double _balance)
+        {
+            name = _name;
+            surname = _surname;
+            patronymic = _patronymic;
+            course = _course;
+            birthday = _birthday;
+            scholarship = _scholarship;
+            balance = _balance;
+
+            marks = new int[N_OF_SUBJECTS];
+        }
+
+        /************************************************/
+
+        public void AddScholarship()
+        {
+            if (Scholarship.IsAvailable(marks))
+            {
+                balance += scholarship;
+            }
+            else
+            {
+                throw new ArgumentException("Unable to add scholarship: average mark is smaller than 4");
+            }
+        }
+
+        public double Withdraw(double _sum)
+        {
+            if (balance < _sum)
+            {
+                throw new ArgumentOutOfRangeException("Specified sum is exceeds balance", "_sum");
+            }
+            else
+            {
+                balance -= _sum;
+                return _sum;
+            }
+        }
+
+        public void SetMark(int _subject_id, int _value)
+        {
+            Mark.CheckIsValid(_value);
+
+            try
+            {
+                marks.SetValue(_value, _subject_id);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        // FIXME: Review return value
+        public object GetMark(int _subject_id)
+        {
+            try {
+                return marks.GetValue(_subject_id);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void RaiseCourse()
+        {
+            ++ course;
+        }
+
+        public void Deduct()
+        {
+            course = 0;
+            Array.Clear(marks, 0, marks.Length);
+        }
+
+        /************************************************/
+
+        public static bool operator == (UniversityStudent _obj1, UniversityStudent _obj2)
+        {
+            return (
+                   _obj1.name       == _obj2.name
+                && _obj1.surname    == _obj2.surname
+                && _obj1.patronymic == _obj2.patronymic
+                && _obj1.marks.SequenceEqual(_obj2.marks)
+                );
+        }
+
+        public static bool operator != (UniversityStudent _obj1, UniversityStudent _obj2)
+        {
+            return !(_obj1 == _obj2);
+        }
+
+        
+        /************************************************/
+    }
+}
---
+++ lab2_v2_new/lab2_v2_new/UniversityStudentTest.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace lab2_v2_new
+{
+    [TestFixture]
+    public class UniversityStudentTest
+    {
+        public const string name = "Ivan";
+        public const string patronymic = "Ivanovich";
+        public const string surname = "Ivanov";
+        public const uint course = 1;
+        public DateTime birthday = new DateTime(1998, 1, 1);
+        public const double scholarship = 998.88;
+        public const double balance = 0.0;
+
+        private void FillAllMarks(ref UniversityStudent _student, int _value)
+        {
+            for (int i = 0; i < UniversityStudent.N_OF_SUBJECTS; i++)
+            {
+                _student.SetMark(i, _value);
+            }
+        }
+
+        private void FillAllMarksDifferent(ref UniversityStudent _student)
+        {
+            for (int i = 0; i < UniversityStudent.N_OF_SUBJECTS; i++)
+            {
+                _student.SetMark(i, i % 6);
+            }
+        }
+
+        private void CheckAllMarksDifferentWritten(ref UniversityStudent _student)
+        {
+            for (int i = 0; i < UniversityStudent.N_OF_SUBJECTS; i++)
+            {
+                Assert.AreEqual(_student.GetMark(i), i % 6);
+            }
+        }
+
+        [Test]
+        public void InitNormal()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );   
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InitNegativeScholarship()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, -100, balance
+            );
+        }
+
+        [Test]
+        public void SetValidMark()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            const int pos = 0, value = 5;
+
+            student.SetMark(pos, value);
+
+            Assert.AreEqual(student.GetMark(pos), value);
+        }
+
+        [Test]
+        public void SetAllValidMarks()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarksDifferent(ref student);
+
+            CheckAllMarksDifferentWritten(ref student);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetNegativeMark()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.SetMark(0, -1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetBigMark()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.SetMark(0, 6);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetNegativeMarkIndex()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.SetMark(-1, 5);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetBigMarkIndex()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.SetMark(UniversityStudent.N_OF_SUBJECTS, 5);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetNegativeMarkIndex()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.GetMark(-1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetBigMarkIndex()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.GetMark(UniversityStudent.N_OF_SUBJECTS);
+        }
+
+        [Test]
+        public void AddScholarship()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student, 4);
+
+            student.AddScholarship();
+
+            Assert.AreEqual(balance + scholarship, student.scholarship);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddScholarshipWithBadMarks()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student, 3);
+
+            student.AddScholarship();
+        }
+
+
+        [Test]
+        public void WithdrawAllMoney()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student, 4);
+
+            student.AddScholarship();
+
+            double prev_balance = student.balance;
+
+            double got = student.Withdraw(scholarship);
+
+            Assert.AreEqual(got, scholarship);
+
+            Assert.AreEqual(student.balance, prev_balance - scholarship);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void WithdrawTooMuchMoney()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student, 4);
+
+            student.AddScholarship();
+
+            student.Withdraw(scholarship + 1);
+        }
+
+        [Test]
+        public void RaiseCourse()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.RaiseCourse();
+
+            Assert.AreEqual(student.course, course + 1);
+        }
+
+        [Test]
+        public void Deduct()
+        {
+            UniversityStudent student;
+
+            student = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            student.RaiseCourse();
+
+            FillAllMarks(ref student, 4);
+
+            Assert.AreNotEqual(student.course, 0);
+            
+            for (int i = 0; i < UniversityStudent.N_OF_SUBJECTS; i++)
+            {
+                Assert.AreNotEqual(student.GetMark(i), 0);
+            }
+
+            student.Deduct();
+
+            Assert.AreEqual(student.course, 0);
+
+            for (int i = 0; i < UniversityStudent.N_OF_SUBJECTS; i++)
+            {
+                Assert.AreEqual(student.GetMark(i), 0);
+            }
+
+        }
+
+        [Test]
+        public void Equality()
+        {
+            UniversityStudent student1;
+
+            student1 = new UniversityStudent(
+                name, surname, patronymic, course, birthday, 0, 0
+            );
+
+            FillAllMarks(ref student1, 4);
+
+            UniversityStudent student2;
+
+            student2 = new UniversityStudent(
+                name, surname, patronymic, course, new DateTime(10, 10, 10), 10.0, 10.0
+            );
+
+            FillAllMarks(ref student2, 4);
+
+            Assert.IsTrue(student1 == student2);
+
+        }
+
+        [Test]
+        public void EqualityNotEqualMarks()
+        {
+            UniversityStudent student1;
+
+            student1 = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student1, 4);
+
+            UniversityStudent student2;
+
+            student2 = new UniversityStudent(
+                name, surname, patronymic, course, birthday, scholarship, balance
+            );
+
+            FillAllMarks(ref student2, 4);
+
+            student2.SetMark(0, 5);
+
+            Assert.IsFalse(student1 == student2);
+            Assert.IsTrue(student1 != student2);
+
+        }
+
+    }
+}
