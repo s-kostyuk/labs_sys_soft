4f13e0659353513120578af8c5e2c5f1ffaee5c3
Sergey Kostyuk <contact@s-kostyuk.me>
2016-12-15 00:10:30 +0200

    Comitted unneeded but quite good files

---
+++ hard_to_throw_out/hard_to_throw_out/MutableVersion.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace hard_to_throw_out
+{
+	public class MutableVersion
+	{
+		/**********************************************************************************/
+
+		public MutableVersion ()
+			: this(0, 0, 0, 0)
+		{}
+
+		public MutableVersion( int _major = 0, int _minor = 0, int _revision = 0, int _build = 0 ) {
+			Major = _major;
+			Minor = _minor;
+			Revision = _revision;
+			Build = _build;
+		}
+
+		/**********************************************************************************/
+
+		private void CheckValue(int _value, string _value_name) {
+			if (_value < 0) {
+				throw new ArgumentOutOfRangeException (
+					_value_name, 
+					String.Format("{0} value can't be negative", _value_name)
+				);
+			}
+		}
+
+		/**********************************************************************************/
+
+		int m_major;
+		int m_minor;
+		int m_revision;
+		int m_build;
+
+		/**********************************************************************************/
+
+		public int Major {
+			get {
+				return m_major;
+			}
+
+			set {
+				CheckValue (value, "Major");
+				m_major = value;
+			}
+		}
+
+		public int Minor {
+			get {
+				return m_minor;
+			}
+
+			set {
+				CheckValue (value, "Minor");
+				m_minor = value;
+			}
+		}
+
+		public int Revision {
+			get {
+				return m_revision;
+			}
+
+			set {
+				CheckValue (value, "Revision");
+				m_revision = value;
+			}
+		}
+
+		public int Build {
+			get {
+				return m_build;
+			}
+
+			set {
+				CheckValue (value, "Build");
+				m_build = value;
+			}
+		}
+
+		/**********************************************************************************/
+
+		public static bool operator == (MutableVersion _v1, MutableVersion _v2) {
+			return _v1.Equals(_v2);
+		}
+
+		public static bool operator != (MutableVersion _v1, MutableVersion _v2) {
+			return !( _v1 == _v2 );
+		}
+
+		public static bool operator  < (MutableVersion _v1, MutableVersion _v2) {
+			if (_v1.Major < _v2.Major) {
+				return true;
+			}
+
+			if (_v1.Minor < _v2.Minor) {
+				return true;
+			}
+
+			if (_v1.Revision < _v2.Revision) {
+				return true;
+			}
+
+			return (_v1.Build < _v2.Build);
+		}
+
+		public static bool operator  > (MutableVersion _v1, MutableVersion _v2) {
+			return _v2 < _v1;
+		}
+
+		/**********************************************************************************/
+
+		public override bool Equals (object _o)
+		{
+			MutableVersion p = _o as MutableVersion;
+
+			return Equals (p);
+		}
+
+		public bool Equals (MutableVersion _p)
+		{
+			if ((object)_p == null) {
+				return false;
+			}
+
+			return Major    == _p.Major
+				&& Minor    == _p.Minor
+				&& Revision == _p.Revision
+				&& Build    == _p.Build;
+		}
+
+		/**********************************************************************************/
+
+		public override string ToString ()
+		{
+			return string.Format ("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
+		}
+
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+
+		/**********************************************************************************/
+	}
+
+
+}
+
---
+++ hard_to_throw_out/hard_to_throw_out/Tests/TestMutableVersion.cs
@@ -0,0 +1,218 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace hard_to_throw_out
+{
+	[TestFixture ()]
+	public class TestMutableVersion
+	{
+		
+		#region Test init validity
+
+		[Test ()]
+		public void TestDefaultConstructor ()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			Assert.AreEqual (0, v.Major);
+			Assert.AreEqual (0, v.Minor);
+			Assert.AreEqual (0, v.Revision);
+			Assert.AreEqual (0, v.Build);
+		}
+
+		[Test ()]
+		public void TestConstructorWith1Param ()
+		{
+			MutableVersion v = new MutableVersion (1);
+
+			Assert.AreEqual (1, v.Major);
+			Assert.AreEqual (0, v.Minor);
+			Assert.AreEqual (0, v.Revision);
+			Assert.AreEqual (0, v.Build);
+		}
+
+		[Test ()]
+		public void TestConstructorWith2Params ()
+		{
+			MutableVersion v = new MutableVersion (1, 2);
+
+			Assert.AreEqual (1, v.Major);
+			Assert.AreEqual (2, v.Minor);
+			Assert.AreEqual (0, v.Revision);
+			Assert.AreEqual (0, v.Build);
+		}
+
+		[Test ()]
+		public void TestConstructorWith3Params ()
+		{
+			MutableVersion v = new MutableVersion (1, 2, 3);
+
+			Assert.AreEqual (1, v.Major);
+			Assert.AreEqual (2, v.Minor);
+			Assert.AreEqual (3, v.Revision);
+			Assert.AreEqual (0, v.Build);
+		}
+
+		[Test ()]
+		public void TestConstructorWithAllParams ()
+		{
+			MutableVersion v = new MutableVersion (1, 2, 3, 4);
+
+			Assert.AreEqual (1, v.Major);
+			Assert.AreEqual (2, v.Minor);
+			Assert.AreEqual (3, v.Revision);
+			Assert.AreEqual (4, v.Build);
+		}
+
+		#endregion
+
+		#region Test constructor invariants
+
+		[Test()]
+		public void TestConstructorNegativeMajor()
+		{
+			Assert.That ( () => {
+				new MutableVersion (-1, 2, 3, 4);
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestConstructorNegativeMinor()
+		{
+			Assert.That ( () => {
+				new MutableVersion (1, -2, 3, 4);
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestConstructorNegativeRevision()
+		{
+			Assert.That ( () => {
+				new MutableVersion (1, 2, -3, 4);
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestConstructorNegativeBuild()
+		{
+			Assert.That ( () => {
+				new MutableVersion (1, 2, 3, -4);
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		#endregion
+
+		#region Test setters with valid params
+
+		[Test()]
+		public void TestSetMajor()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			v.Major = 100;
+
+			Assert.AreEqual (100, v.Major);
+		}
+
+		[Test()]
+		public void TestSetMinor()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			v.Minor = 100;
+
+			Assert.AreEqual (100, v.Minor);
+		}
+
+		[Test()]
+		public void TestSetRevision()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			v.Revision = 100;
+
+			Assert.AreEqual (100, v.Revision);
+		}
+
+		[Test()]
+		public void TestSetBuild()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			v.Build = 100;
+
+			Assert.AreEqual (100, v.Build);
+		}
+
+		#endregion
+
+		#region Test setters invariants
+
+		[Test()]
+		public void TestSetNegativeMajor()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			Assert.That ( () => {
+				v.Major = -1;
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestSetNegativeMinor()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			Assert.That ( () => {
+				v.Minor = -1;
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestSetNegativeRevision()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			Assert.That ( () => {
+				v.Revision = -1;
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		[Test()]
+		public void TestSetNegativeBuild()
+		{
+			MutableVersion v = new MutableVersion ();
+
+			Assert.That ( () => {
+				v.Build = -1;
+			},
+				Throws.InstanceOf<ArgumentOutOfRangeException>()
+			);
+		}
+
+		#endregion
+
+		[Test()]
+		public void TestToString()
+		{
+			MutableVersion v = new MutableVersion (11, 0, 1, 22234);
+
+			Assert.AreEqual ("11.0.1.22234", v.ToString());
+		}
+	}
+}
+
