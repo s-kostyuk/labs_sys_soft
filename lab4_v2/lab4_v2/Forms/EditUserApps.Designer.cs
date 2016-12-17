6c53be7f2320b9d1e5d18886e1c8d57147156419
Sergey Kostyuk <contact@s-kostyuk.me>
2016-12-17 16:34:50 +0200

    Added screenshots and missing files

---
+++ lab4_v2/lab4_v2/Forms/EditUserApps.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace lab4_v2.Forms
+{
+    partial class EditUserApps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelUserApps = new System.Windows.Forms.TableLayoutPanel();
+            this.listBoxAllApps = new System.Windows.Forms.ListBox();
+            this.listBoxAllowedApps = new System.Windows.Forms.ListBox();
+            this.buttonAddApp = new System.Windows.Forms.Button();
+            this.buttonRemoveApp = new System.Windows.Forms.Button();
+            this.tableLayoutPanelUserApps.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelUserApps
+            // 
+            this.tableLayoutPanelUserApps.ColumnCount = 3;
+            this.tableLayoutPanelUserApps.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelUserApps.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanelUserApps.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelUserApps.Controls.Add(this.listBoxAllApps, 0, 0);
+            this.tableLayoutPanelUserApps.Controls.Add(this.listBoxAllowedApps, 2, 0);
+            this.tableLayoutPanelUserApps.Controls.Add(this.buttonAddApp, 1, 0);
+            this.tableLayoutPanelUserApps.Controls.Add(this.buttonRemoveApp, 1, 1);
+            this.tableLayoutPanelUserApps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelUserApps.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelUserApps.Name = "tableLayoutPanelUserApps";
+            this.tableLayoutPanelUserApps.RowCount = 3;
+            this.tableLayoutPanelUserApps.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanelUserApps.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanelUserApps.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelUserApps.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanelUserApps.TabIndex = 0;
+            // 
+            // listBoxAllApps
+            // 
+            this.listBoxAllApps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxAllApps.FormattingEnabled = true;
+            this.listBoxAllApps.Location = new System.Drawing.Point(3, 3);
+            this.listBoxAllApps.Name = "listBoxAllApps";
+            this.tableLayoutPanelUserApps.SetRowSpan(this.listBoxAllApps, 3);
+            this.listBoxAllApps.Size = new System.Drawing.Size(111, 255);
+            this.listBoxAllApps.TabIndex = 0;
+            // 
+            // listBoxAllowedApps
+            // 
+            this.listBoxAllowedApps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxAllowedApps.FormattingEnabled = true;
+            this.listBoxAllowedApps.Location = new System.Drawing.Point(170, 3);
+            this.listBoxAllowedApps.Name = "listBoxAllowedApps";
+            this.tableLayoutPanelUserApps.SetRowSpan(this.listBoxAllowedApps, 3);
+            this.listBoxAllowedApps.Size = new System.Drawing.Size(111, 255);
+            this.listBoxAllowedApps.TabIndex = 1;
+            // 
+            // buttonAddApp
+            // 
+            this.buttonAddApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAddApp.Location = new System.Drawing.Point(120, 3);
+            this.buttonAddApp.Name = "buttonAddApp";
+            this.buttonAddApp.Size = new System.Drawing.Size(44, 24);
+            this.buttonAddApp.TabIndex = 2;
+            this.buttonAddApp.Text = ">>";
+            this.buttonAddApp.UseVisualStyleBackColor = true;
+            this.buttonAddApp.Click += new System.EventHandler(this.buttonAddApp_Click);
+            // 
+            // buttonRemoveApp
+            // 
+            this.buttonRemoveApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRemoveApp.Location = new System.Drawing.Point(120, 33);
+            this.buttonRemoveApp.Name = "buttonRemoveApp";
+            this.buttonRemoveApp.Size = new System.Drawing.Size(44, 24);
+            this.buttonRemoveApp.TabIndex = 3;
+            this.buttonRemoveApp.Text = "<<";
+            this.buttonRemoveApp.UseVisualStyleBackColor = true;
+            this.buttonRemoveApp.Click += new System.EventHandler(this.buttonRemoveApp_Click);
+            // 
+            // EditUserApps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanelUserApps);
+            this.Name = "EditUserApps";
+            this.Text = "EditUserApps";
+            this.tableLayoutPanelUserApps.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelUserApps;
+        private System.Windows.Forms.ListBox listBoxAllApps;
+        private System.Windows.Forms.ListBox listBoxAllowedApps;
+        private System.Windows.Forms.Button buttonAddApp;
+        private System.Windows.Forms.Button buttonRemoveApp;
+    }
+}
---
+++ lab4_v2/lab4_v2/Forms/EditUserApps.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4_v2.Forms
+{
+    public partial class EditUserApps : Form
+    {
+
+        /**********************************************************************************/
+
+        public EditUserApps(AppManager _am, string _username)
+        {
+            InitializeComponent();
+
+            m_app_manager = _am;
+            m_username = _username;
+            m_user = _am.Users[_username];
+
+            UpdateListBoxAllApps();
+            UpdateListBoxAllowedApps();
+        }
+
+        /**********************************************************************************/
+
+        private AppManager m_app_manager;
+        private User m_user;
+        private string m_username;
+
+        /**********************************************************************************/
+
+        public void UpdateListBoxAllApps()
+        {
+            listBoxAllApps.Items.Clear();
+
+            foreach (KeyValuePair<string, App> app in m_app_manager.Apps)
+            {
+                listBoxAllApps.Items.Add(app.Key);
+            }
+        }
+
+        /**********************************************************************************/
+
+        public void UpdateListBoxAllowedApps()
+        {
+            listBoxAllowedApps.Items.Clear();
+
+            foreach (string app_name in m_user.Apps)
+            {
+                listBoxAllowedApps.Items.Add(app_name);
+            }
+        }
+
+        /**********************************************************************************/
+
+        private void buttonAddApp_Click(object sender, EventArgs e)
+        {
+            string target_app = listBoxAllApps.SelectedItem as string;
+
+            try
+            {
+                m_app_manager.AllowUsage(target_app, m_username);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Not enougth RAM");
+                return;
+            }
+            listBoxAllowedApps.Items.Add(target_app);
+        }
+
+        /**********************************************************************************/
+
+        private void buttonRemoveApp_Click(object sender, EventArgs e)
+        {
+            string target_app = listBoxAllowedApps.SelectedItem as string;
+
+            m_app_manager.DenyUsage(target_app, m_username);
+            listBoxAllowedApps.Items.Remove(target_app);
+        }
+
+        /**********************************************************************************/
+
+    }
+}
