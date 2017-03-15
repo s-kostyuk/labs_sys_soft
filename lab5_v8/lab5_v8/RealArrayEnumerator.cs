277cdf1f23db23d3fc901306eb34f303c192d5f9
Sergey Kostyuk <contact@s-kostyuk.me>
2017-03-15 13:56:27 +0200

    Added a draft of the 5th lab (1st in the new semester)

---
+++ lab5_v8/lab5_v8/RealArrayEnumerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab5_v8
+{
+	class RealArrayEnumerator : ISortedRealEnumerator
+	{
+		private double[] m_array;
+		private int index = 0;
+
+		public RealArrayEnumerator (double[] _array)
+		{
+			this.m_array = _array;
+		}
+
+		public double next() {
+			return this.m_array[index++];
+		}
+
+		public double item() {
+			return this.m_array[index];
+		}
+
+		public bool is_end {
+			get {
+				return this.index >= this.m_array.Length || this.m_array.Length == 0;
+			}
+		}
+
+	}
+}
+
---
+++ lab5_v8/lab5_v8/TestRealMerger.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace lab5_v8
+{
+	[TestFixture ()]
+	public class TestRealMerger
+	{
+		[Test ()]
+		public void TestMerge ()
+		{
+			double[] test_array1 = { -1, 2, 3, 6, 7, 8, 9, 10, 66, 88, 89};
+			double[] test_array2 = { -22, 2, 11, 23, 78, 90 };
+
+			double[] exp_result = { -22, -1, 2, 2, 3, 6, 7, 8, 9, 10, 11, 23, 66, 78, 88, 89, 90};
+
+			double[] result = new double[exp_result.Length];
+			int result_index = 0;
+
+			RealMerger merger = new RealMerger ();
+			merger.SetWriter ((double new_value) => {
+				result [result_index++] = new_value;
+			}
+			);
+
+			merger.Merge (ref test_array1, ref test_array2);
+
+			Assert.AreEqual (exp_result, result);
+		}
+	}
+}
+
