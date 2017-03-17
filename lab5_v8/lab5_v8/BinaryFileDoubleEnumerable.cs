d01272d01f9fb92dc0b6450de9fcd23bd7e6287c
Sergey Kostyuk <contact@s-kostyuk.me>
2017-03-17 13:38:18 +0200

    Added an Enumerable for binary files with double numbers

---
+++ lab5_v8/lab5_v8/BinaryFileDoubleEnumerable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace lab5_v8
+{
+	public class BinaryFileDoubleEnumerator : IEnumerator
+	{
+		private BinaryReader m_reader;
+		private double m_current;
+		private bool m_is_got_first = false;
+		private bool m_elements_left = true;
+
+		public BinaryFileDoubleEnumerator(Stream input)
+		{
+			m_reader = new BinaryReader (input);
+		}
+
+		public bool MoveNext() {
+			this.m_is_got_first = true;
+
+			if (this.m_elements_left) {
+				try {
+					this.m_current = this.m_reader.ReadDouble ();
+				}
+				catch (EndOfStreamException) {
+					this.m_elements_left = false;
+				}
+			}
+
+			return this.m_elements_left;
+		}
+
+		public object Current {
+			get {
+				if (!this.m_is_got_first) {
+					throw new InvalidOperationException ("MoveNext() must be called at least once");
+				}
+
+				if (!this.m_elements_left) {
+					throw new InvalidOperationException ("End of file");
+				}
+
+				return this.m_current;
+			}
+		}
+
+		public void Reset()
+		{
+			this.m_reader.BaseStream.Position = 0;
+		}
+
+	}
+
+	public class BinaryFileDoubleEnumerable : IEnumerable
+	{
+		private string m_filename;
+
+		public BinaryFileDoubleEnumerable (string fileName)
+		{
+			this.m_filename = fileName;
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return new BinaryFileDoubleEnumerator (
+				File.Open (this.m_filename, FileMode.Open)
+			);
+		}
+	}
+}
+
---
+++ lab5_v8/lab5_v8/Tests/TestBinaryFileDoubleEnumerator.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace lab5_v8
+{
+	[TestFixture ()]
+	public class TestBinaryFileDoubleEnumerator
+	{
+		// https://msdn.microsoft.com/en-us/library/system.io.binaryreader.readdouble(v=vs.110).aspx
+		[Test ()]
+		public void TestIterations ()
+		{
+			int i;
+			const int arrayLength = 1000;
+
+			// Create random data to write to the stream.
+			Random randomGenerator = new Random();
+			double[] dataArray = new double[arrayLength];
+			for(i = 0; i < arrayLength; i++)
+			{
+				dataArray[i] = 100.1 * randomGenerator.NextDouble();
+			}
+
+			using (BinaryWriter binWriter = 
+				      new BinaryWriter (new MemoryStream ())) 
+			{
+				// Write the data to the stream.
+				Console.WriteLine ("Writing data to the stream.");
+				for (i = 0; i < arrayLength; i++) {
+					binWriter.Write (dataArray [i]);
+				}
+
+				// Create a reader using the stream from the writer.
+				BinaryFileDoubleEnumerator enumer = 
+					new BinaryFileDoubleEnumerator (binWriter.BaseStream);
+
+				// Return to the beginning of the stream.
+				enumer.Reset ();
+
+				for (i = 0; i < arrayLength; i++) {
+					enumer.MoveNext ();
+
+					Assert.AreEqual (dataArray [i], enumer.Current);
+				}
+			}
+		}
+	}
+}
+
