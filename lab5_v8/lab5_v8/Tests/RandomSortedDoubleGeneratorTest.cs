6274d89ea2375df97db3c87512a6d1965e2e10d4
Sergey Kostyuk <contact@s-kostyuk.me>
2017-03-17 14:28:13 +0200

    Added a draft of generator of random sorted double collections

---
+++ lab5_v8/lab5_v8/Tests/RandomSortedDoubleGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace lab5_v8
+{
+	[TestFixture ()]
+	public class RandomSortedDoubleGeneratorTest
+	{
+		[Test ()]
+		public void TestGeneratedArrayIsSorted ()
+		{
+			RandomSortedDoubleGenerator generator = new RandomSortedDoubleGenerator();
+
+			const double min_value = 0;
+			double old_item = min_value;
+
+			double[] generated = generator.Generate(min_value, 100, 1000);
+
+			foreach (double item in generated) {
+				Assert.Greater (item, old_item);
+			}
+		}
+	}
+}
+
