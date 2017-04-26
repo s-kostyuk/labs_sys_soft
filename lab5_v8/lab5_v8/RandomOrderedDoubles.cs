44f8d3e40319a1c1ccdc803fa7075f930da7c671
Sergey Kostyuk <contact@s-kostyuk.me>
2017-04-27 00:53:42 +0300

    Simplified ordered random double sequence generator and added comments

---
+++ lab5_v8/lab5_v8/RandomOrderedDoubles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab5_v8
+{
+	public static class RandomOrderedDoubles
+	{
+		private static double get_next(Random random, double current, double multiplier) {
+			return current + multiplier * random.NextDouble();
+		}
+
+		/// <summary>
+		/// Гегератор последовательности случайно увеличивающихся значений
+		/// </summary>
+		/// <param name="min">Минимальное значение</param>
+		/// <param name="multiplier">Множитель (максимальный шаг)</param>
+		/// <param name="nOfNumbers">Количество генерируемых чисел</param>
+		public static IEnumerable<double> Generator(double min, double multiplier, int nOfNumbers) {
+			double current;
+			Random random = new Random();
+
+			for(int i = 0; i < nOfNumbers; i++)
+			{
+				current = get_next(random, min, multiplier);
+				yield return current;
+			}
+		}
+
+	}
+}
+
--- lab5_v8/lab5_v8/Tests/RandomSortedDoubleGeneratorTest.cs
+++ lab5_v8/lab5_v8/Tests/RandomSortedDoubleGeneratorTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace lab5_v8
 {
@@ -9,14 +10,12 @@ namespace lab5_v8
 		[Test ()]
 		public void TestGeneratedArrayIsSorted ()
 		{
-			RandomSortedDoubleGenerator generator = new RandomSortedDoubleGenerator();
-
 			const double min_value = 0;
 			double old_item = min_value;
 
-			double[] generated = generator.Generate(min_value, 100, 1000);
+			IEnumerable<double> generator = (RandomOrderedDoubles.Generator(min_value, 100, 1000));
 
-			foreach (double item in generated) {
+			foreach (double item in generator) {
 				Assert.Greater (item, old_item);
 			}
 		}
